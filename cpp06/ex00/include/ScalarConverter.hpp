da4cc04090afe750435b92c621d1a1cc90b71d48
Kouta Sakatsume <kosakats@c6r1s12.42tokyo.jp>
2025-11-05 13:25:06 +0900

    update

--- cpp06/ex00/include/ScalarConverter.hpp
+++ cpp06/ex00/include/ScalarConverter.hpp
@@ -6,24 +6,57 @@
 /*   By: kosakats <kosakats@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 19:19:02 by kosakats          #+#    #+#             */
-/*   Updated: 2025/10/28 20:22:56 by kosakats         ###   ########.fr       */
+/*   Updated: 2025/11/04 19:40:25 by kosakats         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
-#include <string>
 #include <iostream>
+#include <string>
+#include <limits>
+#include <cstdlib>
+#include <iomanip>
+#include <cmath>
 
 class ScalarConverter
 {
+public:
+    // --- メインの変換関数（唯一の公開メソッド） ---
+    
+    static void convert(const std::string &literal);
+    // --- 型判定に使う列挙型 ---
+    enum Type {
+        TYPE_CHAR,
+        TYPE_INT,
+        TYPE_FLOAT,
+        TYPE_DOUBLE,
+        TYPE_INVALID
+    };
+    static Type detectType(const std::string &literal);
+
 private:
+    // --- コンストラクタ系（インスタンス化禁止） ---
     ScalarConverter();
+    ScalarConverter(const ScalarConverter &);
+    ScalarConverter &operator=(const ScalarConverter &);
     ~ScalarConverter();
 
-public:
-    static void convert(const std::string &literal);
+    // --- 型判定 ---
+
+    // --- 疑似リテラル判定 (nan, infなど) ---
+    static bool isPseudoLiteral(const std::string &literal);
+
+    // --- 各型から変換処理 ---
+    static void convertFromChar(char c);
+    static void convertFromInt(int n);
+    static void convertFromFloat(float f);
+    static void convertFromDouble(double d);
+
+    // --- 変換結果の出力 ---
+    static void printResult(char c, int i, float f, double d,
+                            bool impossibleFlags[4]);
 };
 
 #endif
--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -6,14 +6,32 @@
 /*   By: kosakats <kosakats@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 19:50:11 by kosakats          #+#    #+#             */
-/*   Updated: 2025/10/29 11:48:34 by kosakats         ###   ########.fr       */
+/*   Updated: 2025/11/04 19:40:42 by kosakats         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
+static bool isPseudoLiteral(const std::string &literal)
+{
+    if (literal == "nan"  || literal == "nanf"  ||
+    literal == "+inf" || literal == "+inff" ||
+    literal == "-inf" || literal == "-inff")
+        return true;
+    else
+        return false;
+}
+
+ScalarConverter::Type detectType(const std::string &literal)
+{
+    if(isPseudoLiteral(literal) == 0)
+    {
+        return ;
+    }
+}
+
 void ScalarConverter::convert(const std::string &literal)
 {
     std::cout << "Converting: " << literal << std::endl;
+    detectType(literal);
 }
-
