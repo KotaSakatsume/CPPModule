c1c37c7bea77707ba5a9a9321fb8070e457d7834
Kouta Sakatsume <kosakats@c6r1s12.42tokyo.jp>
2025-11-14 19:26:14 +0900

    update

--- cpp07/ex00/include/whatever.hpp
+++ cpp07/ex00/include/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: kosakats <kosakats@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 19:37:06 by kosakats          #+#    #+#             */
-/*   Updated: 2025/11/10 20:05:29 by kosakats         ###   ########.fr       */
+/*   Updated: 2025/11/14 19:24:17 by kosakats         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,32 @@
 #include <iostream>
 
 template <typename T>
-void swap(T& a, T& b);
+void swap(T& a, T& b)
+{
+    T temp = a;
+    a = b;      
+    b = temp;   
+}
 
 template <typename T>
-const T& min(const T& a, const T& b);
+const T& min(const T& a, const T& b)
+{
+    return (a < b) ? a : b;
+}
 
 template <typename T>
-const T& max(const T& a, const T& b);
+const T& max(const T& a, const T& b)
+{
+    return (a > b) ? a : b;
+}
+
+template void swap<int>(int&, int&);
+template const int& min<int>(const int&, const int&);
+template const int& max<int>(const int&, const int&);
+
+template void swap<std::string>(std::string&, std::string&);
+template const std::string& min<std::string>(const std::string&, const std::string&);
+template const std::string& max<std::string>(const std::string&, const std::string&);
+
 
 #endif
